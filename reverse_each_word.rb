9d3b235e75f67b9f2a0a1bdd70f5a50498aeb2f1
Steven Topazio <sat@northpointprinting.com>
2020-05-13 18:35:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   words_array = sentence.split(" ")
-  new array = []
-  new array = words_array.collect {|word| word.reverse}
-  new_array.join
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse}
+  new_array.join(" ")
+end
